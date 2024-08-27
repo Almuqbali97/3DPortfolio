132832b5318b0d8b40c27158ecf774e0965c8bf6
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-08-27 15:21:29 +0400

    test computer scence

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,31 +8,31 @@ const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
-    // <mesh>
-    //   <hemisphereLight intensity={1.5} groundColor='black' />
-    //   <spotLight
-    //     position={[-20, 50, 10]}
-    //     angle={0.12}
-    //     penumbra={1}
-    //     intensity={1}
-    //     castShadow
-    //     shadow-mapSize={1024}
-    //   />
-    //   <pointLight intensity={1} />
-    //   <primitive
-    //     object={computer.scene}
-    //     scale={isMobile ? 0.7 : 0.75}
-    //     position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-    //     rotation={[-0.01, -0.2, -0.1]}
-    //   />
-    // </mesh>
-    // <primitive object={computer.scene} scale={1} position-y={-4} position-x={0} position-z={0} rotation-y={0} />
+    <mesh>
+      <hemisphereLight intensity={1.5} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
+    // </mesh>
+    // <primitive object={computer.scene} scale={1} position-y={-4} position-x={0} position-z={0} rotation-y={0} />
+    // <primitive
+    //   object={computer.scene}
+    //   scale={isMobile ? 0.7 : 0.75}
+    //   position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+    //   rotation={[-0.01, -0.2, -0.1]}
+    // />
   );
 };
 
@@ -88,8 +88,8 @@ const ComputersCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
       camera={{
         fov: 25,
-        near: 0.1,
-        far: 200,
+        // near: 0.1,
+        // far: 200,
         position: [20, 3, 5],
       }}
     >
